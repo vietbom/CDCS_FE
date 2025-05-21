784531a1c7b863984a6a1d801919fec963525bb7
ducvietdev <vietnho2004@gmail.com>
2025-05-21 14:02:39 +0700

    pto5

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -7,8 +7,8 @@
 
 import axios from 'axios';
 
-const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+export const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8017',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
@@ -38,5 +38,3 @@ axiosInstance.interceptors.response.use(
         return Promise.reject(error);
     }
 );
-
-export default axiosInstance; 
