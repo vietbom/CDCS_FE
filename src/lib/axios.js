b3d1ea58159873dd257bbd385483e84ff596228b
ducvietdev <vietnho2004@gmail.com>
2025-05-21 14:18:51 +0700

    pro5

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,7 +4,6 @@
 //     baseURL: "http://localhost:8017",
 //     withCredentials:true
 // })
-
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
@@ -15,25 +14,39 @@ export const axiosInstance = axios.create({
     }
 });
 
+// Request interceptor
 axiosInstance.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem('token');
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
+        console.log('Making request to:', config.url);
         return config;
     },
     (error) => {
+        console.error('Request Error:', error);
         return Promise.reject(error);
     }
 );
 
+// Response interceptor
 axiosInstance.interceptors.response.use(
-    (response) => response,
+    (response) => {
+        console.log('Response received:', response.status); 
+        return response;
+    },
     (error) => {
-        if (error.response?.status === 401) {
-            localStorage.removeItem('token');
-            window.location.href = '/login';
+        if (error.response) {
+            console.error('Response Error:', {
+                status: error.response.status,
+                data: error.response.data,
+                headers: error.response.headers
+            });
+        } else if (error.request) {
+            console.error('No response received:', error.request);
+        } else {
+            console.error('Error setting up request:', error.message);
         }
         return Promise.reject(error);
     }
